d1e03dc527863efe1d9fe7a02b0419afce9b725d
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-02 21:39:50 +0300

    Api data fetch

--- js/index.js
+++ js/index.js
@@ -1,32 +1,35 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let departureDropdown = document.querySelector("#departure")
-    let destinationDropdown = document.querySelector("#destination")
+    let arrivalDropdown = document.querySelector("#destination")
     const viewButton = document.getElementById('viewButton');
     let ApiUrl= 'https://www.bcferriesapi.ca/api/';
     
     //fetch data from API
     async function wholeSchedule(){
-        departureDropdown.addEventListener('change', function() {
-            // Get the selected value from the departure dropdown
-            const selectedDeparture = departureDropdown.value;
-        });
-        destinationDropdown.addEventListener('change', function() {
-            // Get the selected value from the destination dropdown
-            const selectedDestination = destinationDropdown.value;
-        });
-        await fetch(ApiUrl)
-        .then(response=>response.json())
-        .then(data=> {
-            // for(let index= 0; index<=data.schedule.length; index++){
-            //     const scheduleUl= document.getElementById("scheduleUl")
-            //     const schedule= document.createElement("li")
-            //     schedule.className = "scheduleLi"
-            //     schedule.innerHTML= `
-            //     <p>Schedule from ${data.schedule[index].}</p>
-            //     `
-            // };
+        fetch(ApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data);
+
+            departureDropdown.addEventListener("change", function(){
+            let departureValue= departureDropdown.value;
+            arrivalDropdown.addEventListener("change", ()=> {
+                let arrivalValue = arrivalDropdown.value;
+                const divList = document.createElement("div")
+                const itemsDiv = document.querySelector("#scheduleUl")
+                divList.className= "destinationList"
+                divList.innerHTML= `
+                <p>Time travel from: ${data.schedule[departureValue][arrivalValue].sailings[1].time} </p>
+                `
+                itemsDiv.appendChild(divList)
+            })
+            // console.log(departureValue)
+            
+            })
+            
+            
         })
-        .catch(error => console.error("Unable to fetch", error));
+        .catch(error => console.error('Error:', error));
     }
     
     function buttonEvent(){
